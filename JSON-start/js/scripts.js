edb98496a78e7a2c81364727a9831169999a9d72
vk-arun <arunvkmonu@gmail.com>
2021-10-25 15:20:18 +0530

    u

--- JSON-start/js/scripts.js
+++ JSON-start/js/scripts.js
@@ -4,6 +4,6 @@ then(mydata=> mydata.json())
 .then(mydata => {
     console.log(mydata);
     console.log(mydata.name);
-    document.getElementById("txt").innerHTML = mydata.name;
+    document.getElementById("pop").innerHTML = mydata.name;
 
 }).catch(err => console.log(err));
