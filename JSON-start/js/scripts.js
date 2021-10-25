bc80d5891e3798d5d57e569cf9d39c6c44b4a284
vk-arun <arunvkmonu@gmail.com>
2021-10-25 15:18:49 +0530

    fudf

--- JSON-start/js/scripts.js
+++ JSON-start/js/scripts.js
@@ -4,5 +4,6 @@ then(mydata=> mydata.json())
 .then(mydata => {
     console.log(mydata);
     console.log(mydata.name);
+    document.getElementById("txt").innerHTML = mydata.name;
 
 }).catch(err => console.log(err));
