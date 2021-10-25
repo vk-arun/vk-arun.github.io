29b5625437860f50d6f2025ac2e5d8ece1c9568b
vk-arun <arunvkmonu@gmail.com>
2021-10-25 15:29:20 +0530

    kjk

--- JSON-start/js/scripts.js
+++ JSON-start/js/scripts.js
@@ -6,4 +6,8 @@ then((mydata)=> mydata.json())
     console.log(mydata.name);
     document.getElementById("pop").innerHTML = mydata.name;
 
+    let sample = document.createElement("h1");
+    sample.textContent = mydata.name;
+    document.getElementById("pop").appendChild(sample)
+
 }).catch((err) => console.log(err));
