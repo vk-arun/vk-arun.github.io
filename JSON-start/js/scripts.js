beb60fc69cbc3e18265e38ba5bff6c4040ad47ca
vk-arun <arunvkmonu@gmail.com>
2021-10-25 15:29:50 +0530

    kk

--- JSON-start/js/scripts.js
+++ JSON-start/js/scripts.js
@@ -8,6 +8,6 @@ then((mydata)=> mydata.json())
 
     let sample = document.createElement("h1");
     sample.textContent = mydata.name;
-    document.getElementById("pop").appendChild(sample)
+    document.getElementById("pop").appendChild(sample);
 
 }).catch((err) => console.log(err));
