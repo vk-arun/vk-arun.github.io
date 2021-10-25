6a7d4aa37441a86d2747b470850921b202aa0015
vk-arun <arunvkmonu@gmail.com>
2021-10-25 15:24:49 +0530

    cfgh

--- JSON-start/js/scripts.js
+++ JSON-start/js/scripts.js
@@ -1,9 +1,9 @@
 
 fetch("testdata.json").
-then(mydata=> mydata.json())
-.then(mydata => {
+then((mydata)=> mydata.json())
+.then((mydata) => {
     console.log(mydata);
     console.log(mydata.name);
     document.getElementById("pop").innerHTML = mydata.name;
 
-}).catch(err => console.log(err));
+}).catch((err) => console.log(err));
